6e46651df37e257b4f7f5f86cc493d921b66d754
KimYura <kimyura1995@mail.ru>
2020-10-05 00:24:44 +0300

    UPD: !

--- django_shorting_url/django_shorting_url/settings.py
+++ django_shorting_url/django_shorting_url/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-#i!%6&&-byxt79(sh#gn=i=xuwz$*hz$g%uo94oxht(k*nsk$'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
